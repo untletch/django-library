e4478960b390fafb5c2f6381183bece3c6bd1985
untletch <untletch@gmail.com>
2022-11-03 15:39:32 +0300

    added to dev branch

--- books/models.py
+++ books/models.py
@@ -7,5 +7,5 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
 
-    def __str_(self):
+    def __str__(self):
         return self.title
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",
     # Local
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
