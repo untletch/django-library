48a8b390732ada3467e2bf02c9012e05063a1152
untletch <untletch@gmail.com>
2022-11-08 19:34:28 +0300

    toggle debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
