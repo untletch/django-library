7626698b53ba1aad3e0324bbd771ec654f429662
untletch <untletch@gmail.com>
2022-11-09 11:36:52 +0300

    added dj_database_url

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,3 +142,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+prod_db = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(prod_db)
